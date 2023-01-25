4d22cb732d718fb429ccb51ed5cb76d430070083
dankuri <danek.il420@gmail.com>
2023-01-26 00:13:01 +0100

    add check for out of range index

--- please/please.py
+++ please/please.py
@@ -175,11 +175,20 @@ def move(old_index: int, new_index: int):
 
 @app.command(short_help="Edit task name")
 def edit(index: int, new_name: str) -> None:
+    if not 0 <= index - 1 < len(config["tasks"]):
+        center_print(
+            "Are you sure you gave me the correct task number?",
+            COLOR_WARNING,
+            wrap=True,
+        )
+        return
+
     if (len(config["tasks"]) == 0):
         center_print(
             "Sorry, cannot edit tasks as the Task list is empty", COLOR_ERROR
         )
         return
+
     if (len(new_name) == 0):
         center_print(
             "Please enter a valid name", COLOR_ERROR
